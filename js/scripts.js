8e97f99cb87c02d7db880f9bb77259b4c98157a8
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-11-13 17:20:43 -0500

    adding basic functionality and styles for small screen menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,6 @@
     // mobile nav
     $('[data-res-navigation-activator]').on('click', function() {
         console.log('opening mobile nav');
+        $(this).next('.res-nav-list').toggleClass('res-nav-list--active');
     })
 })();
