910cc5a35a8236439ab7408fbe760850c2d669c0
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-11-14 20:13:02 -0500

    adding mobile nav activator icon and js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
     // mobile nav
     $('[data-res-navigation-activator]').on('click', function() {
         console.log('opening mobile nav');
+        $(this).toggleClass('res-nav-activator--active');
         $(this).next('.res-nav-list').toggleClass('res-nav-list--active');
     })
 })();
